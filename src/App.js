f4538b8d09dadcd0454f6a02c193423fa8807f29
Anderson Lucas <114498251+andersoncsgo@users.noreply.github.com>
2024-08-18 23:23:07 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import whatsappImage from './img/whatsapp-image-2022-09-20-at-17.36.10.webp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img 
+        src={whatsappImage}
+        alt="bora bill"
+      />
+      <h1>BORA !</h1>
+      <hr />
+      <img 
+        src={whatsappImage}
+        alt="bora bill"
+      />
+      <h1>BORA !</h1>
+      <hr />
+      <hr />
+      <img 
+        src={whatsappImage}
+        alt="bora bill"
+      />
+      <h1>BORA !</h1>
     </div>
   );
 }
